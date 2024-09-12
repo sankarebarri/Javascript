5fbe319d31dae636c99ef378adc0bc977f222fa3
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-12 18:38:49 +0000

    workingWithMaps

---
+++ workingWithMaps/arrayMap.js
@@ -0,0 +1,38 @@
+// Return a new array with the square root of all element values:
+const numbers = [4, 9, 16, 25];
+
+const squareRoot = numbers.map((number) => {
+  Math.sqrt(number);
+});
+console.log(squareRoot); // [ undefined, undefined, undefined, undefined ]
+
+const myNewArr = numbers.map(Math.sqrt);
+console.log(myNewArr); // [ 2, 3, 4, 5 ]
+
+// Multiply all the values in an array with 10:
+const numbers1 = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction);
+function myFunction(n) {
+  return n * 10;
+}
+console.log(newArr);
+
+// Get the full name for each person:
+const persons = [
+  { firstname: "Malcom", lastname: "Reynolds" },
+  { firstname: "Kaylee", lastname: "Frye" },
+  { firstname: "Jayne", lastname: "Cobb" },
+];
+
+const getFullName = (object) => {
+  console.log(object.firstname, object.lastname);
+};
+
+persons.map(getFullName);
+
+const getFullNameArray = (item) => {
+  return [item.firstname, item.lastname].join(" ");
+};
+
+const fullname = persons.map(getFullNameArray);
+console.log(fullname);
