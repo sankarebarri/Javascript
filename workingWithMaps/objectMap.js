37adcb7c8699519c54027d21a1f2796050fe0c03
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-14 15:39:14 +0000

    objectMap

--- workingWithMaps/objectMap.js
+++ workingWithMaps/objectMap.js
@@ -20,3 +20,53 @@ contacts.get("Hillary"); // undefined
 contacts.set("Hillary", { phone: "617-555-4321", address: "321 S 2nd St" });
 contacts.delete("Raymond"); // false
 contacts.delete("Jessie"); // true
+
+/* EXAMPLES */
+const myMap = new Map();
+
+const keyString = "a string";
+const keyObj = {};
+const keyFunc = function () {};
+
+// setting the values
+myMap.set(keyString, "value associated with a string");
+myMap.set(keyObj, "value associated with an Object");
+myMap.set(keyFunc, "value associated with a function ");
+
+console.log(myMap.size);
+
+// getting the values
+console.log(myMap.get(keyFunc));
+console.log(myMap.get("a string")); // value associated with 'a string'", because keyString === 'a string'
+
+console.log(myMap.get({})); // undefined, because keyObj !== {}
+console.log(myMap.get(function () {})); // undefined, because keyFunc !== function () {}
+
+// iterating over Map with for ... of
+const myMap1 = new Map();
+myMap1.set(0, "zero");
+myMap1.set(1, "one");
+
+for (const [key, value] of myMap1) {
+  console.log(`${key} = ${value}`);
+}
+
+// printing just the keys
+for (const key of myMap1.keys()) {
+  console.log(key);
+}
+
+// printing just the values
+for (const value of myMap1.values()) {
+  console.log(value);
+}
+
+// using entries
+for (const [k, v] of myMap1.entries()) {
+  console.log(k, v);
+}
+
+// iterating over Map with forEach
+myMap1.forEach((value, key) => {
+  console.log(`${key} = ${value}`);
+});
